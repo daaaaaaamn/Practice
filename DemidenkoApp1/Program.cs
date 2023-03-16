882ac66126412323bb636ef0b50ed31be77ba543
Анатолий Тузовский <tuzovskyaf@gmail.com>
2023-03-16 13:26:04 +0300

    Внесены изменения

--- DemidenkoApp1/Program.cs
+++ DemidenkoApp1/Program.cs
@@ -1,5 +1,7 @@
-﻿string s = Console .ReadLine();
+﻿Console.Write("Введите значение переменной х: ");
+string s = Console .ReadLine();
 double x = Convert.ToDouble(s);
-double F = 2 * Math.Cos(x);
-Console.WriteLine(F);
+//4Sin(x) * 5Cos(x)
+double F = 4 * Math.Sin(x) * 4 * Math.Cos(x);
+Console.WriteLine($"Значение F = 4Sin({x}) * 4Cos({x}) = {F}.");
 Console.ReadKey();
